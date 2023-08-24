49a3134ee3925ea318bb4ef52fa91b1662d0da01
JoySarkarBD <joysarkarbd407@gmail.com>
2023-08-24 21:47:29 +0600

    file removed

--- src/Layout/Layout.jsx
+++ src/Layout/Layout.jsx
@@ -1,21 +1,23 @@
-import { Outlet, useLocation } from "react-router-dom";
+import { Outlet } from "react-router-dom";
 import Footer from "../components/Shared/Footer/Footer";
 import Navbar from "../components/Shared/Navbar/Navbar";
 
 const Layout = () => {
-  const location = useLocation();
-  const noNav = location.pathname.includes("/admin");
-  const noFooter =
-    location.pathname.includes("/admin") ||
-    location.pathname.includes("/work-queue") ||
-    location.pathname.includes("/upload-data") ||
-    location.pathname.includes("/reports");
+  // const location = useLocation();
+  // const noNav = location.pathname.includes("/admin");
+  // const noFooter =
+  //   location.pathname.includes("/admin") ||
+  //   location.pathname.includes("/work-queue") ||
+  //   location.pathname.includes("/upload-data") ||
+  //   location.pathname.includes("/reports");
 
   return (
     <>
-      {noNav || <Navbar />}
+      {/* {noNav || <Navbar />} */}
+      <Navbar />
       <Outlet />
-      {noFooter || <Footer />}
+      <Footer />
+      {/* {noFooter || <Footer />} */}
     </>
   );
 };
