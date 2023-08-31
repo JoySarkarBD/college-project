256e7d92a32b0abead2176dd4f0b0bad50c86ff4
diptowebhero <diptodas1672@gmail.com>
2023-08-31 21:47:40 +0600

    feat: add DELEGATE ACCESS page

--- src/pages/Admin/DelegateAccess.jsx
+++ src/pages/Admin/DelegateAccess.jsx
@@ -1,7 +1,59 @@
+import "./Admin.css";
+import DelegationTable from "./DelegationTable";
 const DelegateAccess = () => {
   return (
-    <div>
-      <h1> DELEGATE ACCESS</h1>
+    <div className="container my-5">
+      <div className="title text-center">
+        <h2>Delegate Access</h2>
+      </div>
+      {/* Delegate Access form */}
+      <form className="delegate_form mt-5">
+        <div className="row">
+          {/* System Effective Date FIELD*/}
+          <div className="col-md-3">
+            <div className="mb-3">
+              <label>System Effective Date</label>
+              <input
+                type="date"
+                className="form-control formInput"
+                placeholder="Steven Walker"
+              />
+            </div>
+          </div>
+          {/* From FIELD*/}
+          <div className="col-md-3">
+            <div className="mb-3">
+              <label>From</label>
+              <input
+                type="date"
+                className="form-control formInput"
+                placeholder="Steven Walker"
+              />
+            </div>
+          </div>
+          {/* TO FIELD*/}
+          <div className="col-md-3">
+            <div className="mb-3">
+              <label>To</label>
+              <input
+                type="date"
+                className="form-control formInput"
+                placeholder="Steven Walker"
+              />
+            </div>
+          </div>
+          {/* button */}
+          <div className="col-md-3">
+            <label></label>
+            <div className="mt-2">
+              <button className="btn btn-delegate">Delegate</button>
+            </div>
+          </div>
+        </div>
+      </form>
+
+      {/* Delegation Access Past & Active table */}
+      <DelegationTable />
     </div>
   );
 };
