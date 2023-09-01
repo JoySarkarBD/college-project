eb65dba1cd17ade96a49ac923ab54684f1ce3d7e
JoySarkarBD <joysarkarbd407@gmail.com>
2023-09-01 15:10:50 +0600

    feat: GoToPage done

--- src/components/Form/GoToInput.jsx
+++ src/components/Form/GoToInput.jsx
@@ -1,15 +1,21 @@
 /* eslint-disable no-unused-vars */
 /* eslint-disable react/prop-types */
+import React, { useState } from "react"; // Import React and useState
 import "./SearchInput.css";
 
 const GoToInput = ({ title, gotoPage, ...attributes }) => {
+  // Initialize state for the input value
+  const [inputValue, setInputValue] = useState(1);
+
   const handleInputChange = (e) => {
     const rawValue = e.target.value;
     let pageNumber = parseInt(rawValue);
 
     if (isNaN(pageNumber) || pageNumber <= 0) {
-      gotoPage(0); // Default to page 1 for non-numeric or negative input
+      setInputValue(""); // Clear the input field for non-numeric or negative input
+      gotoPage(1); // Set to page 1 for non-numeric or negative input
     } else {
+      setInputValue(pageNumber); // Update the input value
       gotoPage(pageNumber - 1); // Adjusted to zero-based index
     }
   };
@@ -22,6 +28,8 @@ const GoToInput = ({ title, gotoPage, ...attributes }) => {
       <input
         className='mx-2 py-1 px-2 w-25 number-inp'
         {...attributes}
+        min={1}
+        value={inputValue} // Use the controlled component value
         onChange={handleInputChange}
       />
     </>
