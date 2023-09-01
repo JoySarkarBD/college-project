a2cc8889ab46d07c0254e974649b83a56562693b
diptowebhero <diptodas1672@gmail.com>
2023-09-01 12:57:24 +0600

    feat: add table edit button

---
+++ src/components/ColumnFilter/EmptyFilter.jsx
@@ -0,0 +1,9 @@
+const emptyFilter = () => {
+  return (
+    <span>
+      <input type="text" className="visually-hidden-focusable" />
+    </span>
+  );
+};
+
+export default emptyFilter;
--- src/components/UserDataTable/UserDataTable.jsx
+++ src/components/UserDataTable/UserDataTable.jsx
@@ -10,9 +10,10 @@ import "./UserDataTable.css";
 
 // this is the column searching function
 import { AiOutlinePlus } from "react-icons/ai";
-import { NavLink } from "react-router-dom";
+import { Link, NavLink } from "react-router-dom";
 import userManageData from "../../../userList.json";
 import { columnFilter } from "../ColumnFilter/ColumnFilter";
+import emptyFilter from "../ColumnFilter/EmptyFilter";
 import GoToInput from "../Form/GoToInput";
 import Pagination from "../Pagination/Pagination";
 import AdminTableTopbar from "../TableTopbar/AdminTableTopbar";
@@ -65,10 +66,24 @@ const UserDataTable = () => {
         accessor: "ticketsAssigned",
         Filter: columnFilter,
       },
+
       {
-        Header: "EDIT",
-        accessor: "", // Assuming you want to display the name
-        Filter: columnFilter,
+        Header: "ACTIONS",
+        accessor: "ACTIONS",
+        Filter: emptyFilter,
+        Cell: ({ row }) => {
+          return (
+            <div>
+              <Link to="/manage-user/modify-user">
+                <button // Define your edit action function
+                  className="btn-edit"
+                >
+                  Edit
+                </button>
+              </Link>
+            </div>
+          );
+        },
       },
     ],
     []
@@ -97,23 +112,23 @@ const UserDataTable = () => {
   );
 
   return (
-    <div className='px-5 my-5 table-responsive'>
-      <div className='row table_top_area'>
+    <div className="px-5 my-5 table-responsive">
+      <div className="row table_top_area">
         {/* Go To Input */}
-        <div className='col-lg-6 col-md-12 col-sm-12 my-2 text-lg-start text-md-center text-sm-center text-center'>
+        <div className="col-lg-6 col-md-12 col-sm-12 my-2 text-lg-start text-md-center text-sm-center text-center">
           <GoToInput
-            type='number'
-            title='Go to page:'
+            type="number"
+            title="Go to page:"
             gotoPage={gotoPage}
             defaultValue={pageIndex + 1}
           />
         </div>
 
-        <div className='col-lg-6 col-md-12 col-sm-12 my-2 text-lg-end text-md-center text-sm-center text-center'>
-          <NavLink to='/manage-user/add-user'>
-            <button className=' border-0 bg-transparent add_user_btn'>
+        <div className="col-lg-6 col-md-12 col-sm-12 my-2 text-lg-end text-md-center text-sm-center text-center">
+          <NavLink to="/manage-user/add-user">
+            <button className=" border-0 bg-transparent add_user_btn">
               Add User
-              <AiOutlinePlus className='user_add_icon' />
+              <AiOutlinePlus className="user_add_icon" />
             </button>
           </NavLink>
         </div>
@@ -122,30 +137,31 @@ const UserDataTable = () => {
       {/* Table topbar */}
       <AdminTableTopbar />
 
-      <table className='table' {...getTableProps()}>
+      <table className="table" {...getTableProps()}>
         {/* Mapping the data of header */}
         <thead>
-          {headerGroups.map((headerGroup, index) => (
+          {headerGroups?.map((headerGroup, index) => (
             <tr key={index} {...headerGroup.getHeaderGroupProps()}>
               {/* select all */}
               <th>
                 <input
-                  type='checkbox'
+                  type="checkbox"
                   checked={selectedRows.length === page.length}
                   onChange={() => {
                     if (selectedRows.length === page.length) {
                       setSelectedRows([]);
                     } else {
-                      setSelectedRows(page.map(row => row.original));
+                      setSelectedRows(page.map((row) => row.original));
                     }
                   }}
                 />
               </th>
-              {headerGroup.headers.map((column, index) => (
+              {headerGroup?.headers?.map((column, index) => (
                 <th
                   key={index}
-                  scope='col'
-                  {...column.getHeaderProps(column.getSortByToggleProps())}>
+                  scope="col"
+                  {...column.getHeaderProps(column.getSortByToggleProps())}
+                >
                   {column.render("Header")}
                   <span>
                     {column.isSorted
@@ -154,7 +170,9 @@ const UserDataTable = () => {
                         : " 🔼"
                       : ""}
                   </span>
-                  <div>{column.canFilter ? column.render("Filter") : null}</div>
+                  <div>
+                    {column?.canFilter ? column?.render("Filter") : null}
+                  </div>
                 </th>
               ))}
             </tr>
@@ -170,21 +188,21 @@ const UserDataTable = () => {
               <tr key={index} {...row.getRowProps()}>
                 <td>
                   <input
-                    type='checkbox'
+                    type="checkbox"
                     checked={selectedRows.some(
-                      selectedRow =>
+                      (selectedRow) =>
                         selectedRow.claimId === row.original.claimId
                     )}
                     onChange={() => {
                       if (
                         selectedRows.some(
-                          selectedRow =>
+                          (selectedRow) =>
                             selectedRow.claimId === row.original.claimId
                         )
                       ) {
                         setSelectedRows(
                           selectedRows.filter(
-                            selectedRow =>
+                            (selectedRow) =>
                               selectedRow.claimId !== row.original.claimId
                           )
                         );
@@ -206,7 +224,7 @@ const UserDataTable = () => {
       </table>
 
       {/* Pagination */}
-      <div className='d-flex align-items-center justify-content-center mt-5'>
+      <div className="d-flex align-items-center justify-content-center mt-5">
         <Pagination
           gotoPage={gotoPage}
           pageSize={pageSize}
